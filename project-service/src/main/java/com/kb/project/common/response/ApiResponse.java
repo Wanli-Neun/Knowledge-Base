961af0a7cc2c2f2da2bdd4d35f1a50c1308184b3
Nguyen Quang Loi <quangloi317@gmail.com>
2026-01-05 15:58:38 +0700

    Feat(ApiResponse): response template

---
+++ project-service/src/main/java/com/kb/project/common/response/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.kb.project.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private int status;
+    private String message;
+    private Object metadata;
+    private T result;
+    
+}
