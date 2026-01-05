d296af64adb83f35e8096f03d9c98afb57f905b5
Nguyen Quang Loi <quangloi317@gmail.com>
2026-01-05 09:10:17 +0700

    fix(AuthService): fix APIs' response

--- auth-service/src/main/java/com/kb/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/kb/auth/controller/AuthController.java
@@ -1,17 +1,18 @@
 package com.kb.auth.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kb.auth.DTO.Request.Auth.LoginRequest;
-import com.kb.auth.DTO.Request.Auth.RefreshRequest;
-import com.kb.auth.DTO.Request.Auth.RegisterRequest;
-import com.kb.auth.DTO.Response.Auth.AuthResponse;
+import com.kb.auth.common.response.ApiResponse;
+import com.kb.auth.dto.request.auth.LoginRequest;
+import com.kb.auth.dto.request.auth.RefreshRequest;
+import com.kb.auth.dto.request.auth.RegisterRequest;
+import com.kb.auth.dto.response.auth.AuthResponse;
 import com.kb.auth.service.AuthService;
+import com.kb.auth.common.response.ApiResponseBuilder;
 
 @RestController
 @RequestMapping("/auth")
@@ -24,35 +25,34 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Void> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<ApiResponse<Void>> register(@RequestBody RegisterRequest request) {
 
         authService.register(request.email(), request.password());
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ApiResponseBuilder.created();
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<ApiResponse<AuthResponse>> login(@RequestBody LoginRequest request) {
 
         AuthResponse response = authService.login(request.email(), request.password());
 
-        return ResponseEntity.ok(response);
+        return ApiResponseBuilder.success("User logged in successfully", response);
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<AuthResponse> refresh(@RequestBody RefreshRequest request) {
-
+    public ResponseEntity<ApiResponse<AuthResponse>> refresh(@RequestBody RefreshRequest request) {
         AuthResponse response = authService.refresh(request.refreshToken());
 
-        return ResponseEntity.ok(response);
+        return ApiResponseBuilder.success("Token refreshed successfully", response);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@RequestBody RefreshRequest request) {
+    public ResponseEntity<ApiResponse<Void>> logout(@RequestBody RefreshRequest request) {
 
         authService.logout(request.refreshToken());
 
-        return ResponseEntity.noContent().build();
+        return ApiResponseBuilder.noContent();
     }
 
 
